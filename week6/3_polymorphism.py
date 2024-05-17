b01bf6bc60a35bf55e610df8896e9361cc1e4ee9
Hyunjae Park <96413678+code2gold@users.noreply.github.com>
2024-05-17 19:23:06 +0900

    feat: add polymorphism and abstraction lecture

---
+++ week6/3_polymorphism.py
@@ -0,0 +1,8 @@
+# Define the Animal class with speak method
+
+# Define the Dog class overriding speak method
+
+# Define the Cat class overriding speak method
+
+# Write a function that takes an Animal as an argument and calls animal.speak() inside it.
+
---
+++ week6/4_abstraction.py
@@ -0,0 +1,9 @@
+# Define the abstract animal class using abc module
+
+# Dog class: implement by inheriting the Animal class
+
+# Cat class: implement by inheriting the Animal class
+
+# Quiz: Define Shape class
+# Inherit Shape class from Circle, Rectangle,Triangle class.
+# Define area method
